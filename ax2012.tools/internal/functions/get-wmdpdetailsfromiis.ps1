53f8c9f3c180ff427bf7be4266ddd85fadd2772a
Mötz Jensen <motz@e-s.dk>
2018-10-07 13:06:41 +0200

    Fix things

--- ax2012.tools/internal/functions/get-wmdpdetailsfromiis.ps1
+++ ax2012.tools/internal/functions/get-wmdpdetailsfromiis.ps1
@@ -1,8 +1,23 @@
-﻿function Get-WMDPDetailsFromIIS {
+﻿<#
+.SYNOPSIS
+Get WMDP details from the IIS
+
+.DESCRIPTION
+Get all the necessary details from the IIS about the WMDP installation
+
+.EXAMPLE
+Get-WMDPDetailsFromIIS
+
+This will get details from all the WMDP installations on the server.
+
+.NOTES
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Get-WMDPDetailsFromIIS {
     [CmdletBinding()]
-    param (
-        
-    )
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    param ( )
 
     if ((Get-Module Webadministration).Count -lt 1) {
         Write-PSFMessage -Level Host -Message "It seems that you didn't have the <c='em'>IIS powershell</c> administration tools installed."
