aac2f51e653f431c41b1cb733acf5bba52e7e1a2
Mötz Jensen <motz@e-s.dk>
2018-10-15 11:50:32 +0200

    Add missing files

---
+++ ax2012.tools/functions/get-axclientbuild.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.SYNOPSIS
+Get the build numbers
+
+.DESCRIPTION
+Get the build numbers for the AX 2012 client
+
+.EXAMPLE
+PS C:\> Get-AxClientBuild
+
+This will get the executable path and the build numbers for the client.
+
+.NOTES
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Get-AxClientBuild {
+    [CmdletBinding()]
+    Param(
+    )
+    
+    $RegKey = Get-Item -Path $Script:RegistryClient
+    
+    $RegOuter = Get-ItemProperty -Path $($RegKey.Name.Replace("HKEY_CURRENT_USER", "HKCU:"))
+
+    $RegInner = Get-ItemProperty -Path (Join-Path $RegKey.Name $RegOuter.Current).Replace("HKEY_CURRENT_USER", "HKCU:")
+
+    $BuildNumbers = Get-FileVersion -Path $(Join-Path $RegInner.bindir "Ax32.exe")
+
+    $clientDetails = [Ordered]@{}
+
+    $clientDetails.ExecutablePath = Join-Path $RegInner.bindir "Ax32.exe"
+
+    $clientDetails.FileVersion = $BuildNumbers.FileVersion
+    $clientDetails.ProductVersion = $BuildNumbers.ProductVersion
+    $clientDetails.FileVersionUpdated = $BuildNumbers.FileVersionUpdated
+    $clientDetails.ProductVersionUpdated = $BuildNumbers.ProductVersionUpdated
+
+    [PSCustomObject] $clientDetails
+}
---
+++ ax2012.tools/functions/get-axclientconfig.ps1
@@ -0,0 +1,57 @@
+﻿<#
+.SYNOPSIS
+Get the AX 2012 client configuration
+
+.DESCRIPTION
+Get the AX 2012 client configuration from the registry
+
+.PARAMETER Name
+Name of the configuration that you are looking for
+
+The parameter supports wildcards. E.g. -Name "*DEV*"
+
+.EXAMPLE
+PS C:\> Get-AxClientConfig
+
+This will get all available client configurations from the registry and display them.
+
+.NOTES
+Author: Mötz Jensen (@Splaxi)
+
+#>
+function Get-AxClientConfig {
+    [CmdletBinding()]
+    Param(
+        [Parameter()]
+        [SupportsWildcards()]
+        [string] $Name = "*"
+    )
+    
+    $configs = Get-ChildItem -Path $Script:RegistryClient
+
+    foreach ($item in $configs) {
+        $RegistryPath = Get-Item -Path $($item.Name.Replace("HKEY_CURRENT_USER", "HKCU:"))
+        $configName = Split-Path -Path $RegistryPath.Name -Leaf
+        
+        if ($configName -NotLike $Name) { continue }
+
+        $RegInner = Get-ItemProperty -Path $($RegistryPath.Name.Replace("HKEY_CURRENT_USER", "HKCU:"))
+        $res = [Ordered]@{ConfigName = $configName}
+        
+        $res.AosServer = ($RegInner.Aos2).Split(":")[0]
+        $res.AosPort = ($RegInner.Aos2).Split(":")[1]
+        
+        if ($RegInner.Aos2 -like "*@*") {
+            $res.InstanceName = ($RegInner.Aos2).Split("@")[0]
+        }
+
+        $res.AosTrafficEncrypted = $RegInner.AosEncryption
+
+        $res.BinDir = $RegInner.BinDir
+        $res.ClientDirectory = $RegInner.Directory
+        $res.Aol = $RegInner.Aol
+        $res.AolCode = $RegInner.AolCode
+        
+        [PSCustomObject]$res
+    }
+}
