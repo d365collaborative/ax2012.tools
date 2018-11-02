ab93127f410034f8f70045d9794f18015d98521d
Mötz Jensen <motz@e-s.dk>
2018-11-02 13:58:32 +0100

    format cbh

--- ax2012.tools/internal/functions/clear-ax2012standardpowershellmodule.ps1
+++ ax2012.tools/internal/functions/clear-ax2012standardpowershellmodule.ps1
@@ -1,19 +1,20 @@
-﻿<#
-.SYNOPSIS
-Clear the imported AX 2012 PowerShell modules
-
-.DESCRIPTION
-Removes the different AX 2012 PowerShell modules that has been loaded into the session.
-
-.EXAMPLE
-PS C:\> Clear-Ax2012StandardPowershellModule
-
-This will remove all the known AX 2012 PowerShell modules that have been loaded.
-It is connected to the use of Import-Module $Script:AxPowerShellModule
-
-.NOTES
-Author: Mötz Jensen (@Splaxi)
-
+﻿
+<#
+    .SYNOPSIS
+        Clear the imported AX 2012 PowerShell modules
+        
+    .DESCRIPTION
+        Removes the different AX 2012 PowerShell modules that has been loaded into the session.
+        
+    .EXAMPLE
+        PS C:\> Clear-Ax2012StandardPowershellModule
+        
+        This will remove all the known AX 2012 PowerShell modules that have been loaded.
+        It is connected to the use of Import-Module $Script:AxPowerShellModule
+        
+    .NOTES
+        Author: Mötz Jensen (@Splaxi)
+        
 #>
 function Clear-Ax2012StandardPowershellModule {
     [CmdletBinding()]
