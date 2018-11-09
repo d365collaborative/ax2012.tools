2f8d8f4d27a304e56f925ac097bbb289fd709420
Mötz Jensen <motz@e-s.dk>
2018-11-09 19:13:55 +0100

    Format CBH

--- ax2012.tools/functions/invoke-axbuild.ps1
+++ ax2012.tools/functions/invoke-axbuild.ps1
@@ -1,49 +1,50 @@
-﻿<#
-.SYNOPSIS
-Start the AxBuild.exe
-
-.DESCRIPTION
-Invoke the AxBuild.exe with the necessary parameters to make it compile your application
-
-.PARAMETER BinDirectory
+﻿
+<#
+    .SYNOPSIS
+        Start the AxBuild.exe
+        
+    .DESCRIPTION
+        Invoke the AxBuild.exe with the necessary parameters to make it compile your application
+        
+    .PARAMETER BinDirectory
         The full path to the bin directory where the AOS instance is physical installed
-
-.PARAMETER AlternativeBinPath
-The full path to the client bin directory where AX 2012 Client is physical installed
-
-.PARAMETER InstanceNumber
+        
+    .PARAMETER AlternativeBinPath
+        The full path to the client bin directory where AX 2012 Client is physical installed
+        
+    .PARAMETER InstanceNumber
         The 2 digit ([0-9][0-9]) number that the AOS instance has on the server
-
+        
     .PARAMETER DatabaseServer
         The name of the server running SQL Server
-
-.PARAMETER ModelstoreDatabase
+        
+    .PARAMETER ModelstoreDatabase
         The name of the AX 2012 modelstore database
-
-.PARAMETER Workers
-Number of workers that you want to utilize while compiling
-
-The built-in logic from AxBuild.exe will choose a number equal to your visible cores
-Leaving it blank or with 0 (Zero) will use the built-in logic from AxBuild.exe
-
-.PARAMETER Log
-Path to the log file you want AxBuild.exe to output to
-
-.EXAMPLE
-PS C:\> Get-AxAosInstance | Invoke-AxBuild
-
-This will find all AOS instances using the Get-AxAosInstance on the machine and pipe them to Invoke-AxBuild.
-For each AOS instance found it will start the AxBuild.exe against their individual details.
-It will store the log file under the default ax2012.tools folder.
-
-.EXAMPLE
-PS C:\> Invoke-AxBuild
-
-This will start the AxBuild.exe against the ActiveAos configuration.
-It will store the log file under the default ax2012.tools folder.
-
-.NOTES
-Author: Mötz Jensen (@Splaxi)
+        
+    .PARAMETER Workers
+        Number of workers that you want to utilize while compiling
+        
+        The built-in logic from AxBuild.exe will choose a number equal to your visible cores
+        Leaving it blank or with 0 (Zero) will use the built-in logic from AxBuild.exe
+        
+    .PARAMETER Log
+        Path to the log file you want AxBuild.exe to output to
+        
+    .EXAMPLE
+        PS C:\> Get-AxAosInstance | Invoke-AxBuild
+        
+        This will find all AOS instances using the Get-AxAosInstance on the machine and pipe them to Invoke-AxBuild.
+        For each AOS instance found it will start the AxBuild.exe against their individual details.
+        It will store the log file under the default ax2012.tools folder.
+        
+    .EXAMPLE
+        PS C:\> Invoke-AxBuild
+        
+        This will start the AxBuild.exe against the ActiveAos configuration.
+        It will store the log file under the default ax2012.tools folder.
+        
+    .NOTES
+        Author: Mötz Jensen (@Splaxi)
 #>
 function Invoke-AxBuild {
     [CmdletBinding()]
