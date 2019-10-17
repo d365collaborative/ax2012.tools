2d58c8b921158e83e34d0c7402a87bf061b4199a
Mötz Jensen <motz@e-s.dk>
2019-10-17 15:26:41 +0200

    Update add-axenvironmentconfig.ps1

--- ax2012.tools/functions/add-axenvironmentconfig.ps1
+++ ax2012.tools/functions/add-axenvironmentconfig.ps1
@@ -1,4 +1,72 @@
-﻿function Add-AxEnvironmentConfig {
+﻿<#
+.SYNOPSIS
+Add configuration details for an entire AX 2012 environment
+
+.DESCRIPTION
+Build a configuration containing all the different servers / machines that is part of any given AX 2012 environment
+
+You could register your TEST, SAT, UAT, PROD environment and easily switch between them when you want to troubleshoot or run maintenance work against them
+
+.PARAMETER Name
+Name of the environment that you are adding
+
+.PARAMETER AosServers
+Array with server names of all the servers that host an AOS instance in the specific environment
+
+.PARAMETER InstanceName
+Name of the instance that is used to uniquely identify the environment across multiple AOS instances
+
+.PARAMETER DatabaseServers
+Array with server names of all the servers that host a SQL Server database for the environment
+
+.PARAMETER Database
+Database name for the SQL Server database that the AOS instance(s) connects to
+
+.PARAMETER ModelstoreDatabase
+Database name for the SQL Server database that holds the modelstore (code)
+
+.PARAMETER SsrsServers
+Array with server names of all the servers that host a Sql Server Reporting Services (SSRS) instance in the specific environment
+
+.PARAMETER EpServers
+Array with server names of all the servers that host a SharePoint installation with corresponding Enterprise Portal components in the specific environment
+
+.PARAMETER WmdpServers
+Array with server names of all the servers that host an IIS installation with corresponding Warehouse Mobile Device Portal (WMDP) components in the specific environment
+
+.PARAMETER Mr2012Servers
+Array with server names of all the servers that host a Management Reporter 2012 instance in the specific environment
+
+.PARAMETER SsasServers
+Array with server names of all the servers that host a Sql Server Analysis Services (SSAS) instance in the specific environment
+
+.PARAMETER Append
+Instructs the cmdlet to append the different parameter values with those that might already exist in the configuration store
+
+.PARAMETER Force
+Instruct the cmdlet to overwrite the specified parameter values in the configuration store
+
+    .PARAMETER Temporary
+        Instruct the cmdlet to only temporarily add the environment configuration in the configuration store
+
+        Great help while building the configuration and you don't want to persist the configuration on the machine
+
+.EXAMPLE
+PS C:\> Add-AxEnvironmentConfig -Name AXTEST -AosServers TESTAOS01 -InstanceName AXTEST -DatabaseServers TESTSQL01 -Database AXTEST -ModelstoreDatabase AXTEST_model -Temporary
+
+This adds a new environment configuration to the configuration store.
+The Name AXTEST is used as the name for the configuration of the environment.
+The InstanceName AXTEST is used as the instance name for the configuration of the environment.
+The server TESTAOS01 is registered as the AOS Server.
+The server TESTSQL01 is registered as the SQL Server.
+The database AXTEST is registered as the SQL Server database.
+The database AXTEST_model is registered as the SQL Server database for the modelstore.
+
+.NOTES
+
+#>
+
+function Add-AxEnvironmentConfig {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
@@ -6,7 +74,13 @@
 
         [string[]] $AosServers,
 
+        [string] $InstanceName,
+
         [string[]] $DatabaseServers,
+
+        [string] $Database,
+
+        [string] $ModelstoreDatabase,
 
         [string[]] $SsrsServers,
 
