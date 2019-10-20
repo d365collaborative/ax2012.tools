dc716d17dd9fb10fa5287272c63ab0a95a431943
Mötz Jensen <motz@e-s.dk>
2019-10-20 13:11:43 +0200

    Docs: CBH formatted

--- ax2012.tools/functions/get-axserverxppilhash.ps1
+++ ax2012.tools/functions/get-axserverxppilhash.ps1
@@ -1,50 +1,51 @@
-﻿<#
-.SYNOPSIS
-Get hash values for XppIL files
-
-.DESCRIPTION
-Generate the file hash values for each *.netmodule & *.dll file in the XppIL folder
-
+﻿
+<#
+    .SYNOPSIS
+        Get hash values for XppIL files
+        
+    .DESCRIPTION
+        Generate the file hash values for each *.netmodule & *.dll file in the XppIL folder
+        
     .PARAMETER InstanceName
         Name of the instance that you are working against
         
         Default value can be configured with the Set-AxActiveAosConfig cmdlet
-
-.PARAMETER OutputPath
-Path to the folder where the output file must be saved
-
-Default value is: "C:\temp\ax2012.tools"
-
-.PARAMETER FileName
-Name of the output as you want it to be named
-
-If left empty the output file will be named based on the following pattern: "SERVERNAME_INSTANCENAME_XppIL_HashValue.txt"
-
-.EXAMPLE
-PS C:\> Get-AxServerXppILHash -InstanceName "AXTEST"
-
-This will generate the file hash values for the XppIL files for the AXTEST AOS Instance.
-It will work against the Instance AXTEST.
-It will save the output file to the default folder location "C:\temp\ax2012.tools".
-The file will named "SERVER_AXTEST_XppIL_HashValues.txt"
-
-.EXAMPLE
-PS C:\> Get-AxServerXppILHash
-
-This will generate the file hash values for the XppIL files for the default AOS Instance.
-It will work against the default AOS Instance that has been configured with Set-AxActiveAosConfig.
-It will save the output file to the default folder location "C:\temp\ax2012.tools".
-The file will named "SERVER_INSTANCENAME_XppIL_HashValues.txt"
-
-The default value of the instance name can be configured with the Set-AxActiveAosConfig cmdlet.
-
-.NOTES
-Tags: XppIL, Hash, Files
-
-Author: Mötz Jensen (@Splaxi)
-
-All credits goes to Kenneth Madsen (@KennethGrupp) for providing detailed examples on how to achieve this the best way using powershell
-
+        
+    .PARAMETER OutputPath
+        Path to the folder where the output file must be saved
+        
+        Default value is: "C:\temp\ax2012.tools"
+        
+    .PARAMETER FileName
+        Name of the output as you want it to be named
+        
+        If left empty the output file will be named based on the following pattern: "SERVERNAME_INSTANCENAME_XppIL_HashValue.txt"
+        
+    .EXAMPLE
+        PS C:\> Get-AxServerXppILHash -InstanceName "AXTEST"
+        
+        This will generate the file hash values for the XppIL files for the AXTEST AOS Instance.
+        It will work against the Instance AXTEST.
+        It will save the output file to the default folder location "C:\temp\ax2012.tools".
+        The file will named "SERVER_AXTEST_XppIL_HashValues.txt"
+        
+    .EXAMPLE
+        PS C:\> Get-AxServerXppILHash
+        
+        This will generate the file hash values for the XppIL files for the default AOS Instance.
+        It will work against the default AOS Instance that has been configured with Set-AxActiveAosConfig.
+        It will save the output file to the default folder location "C:\temp\ax2012.tools".
+        The file will named "SERVER_INSTANCENAME_XppIL_HashValues.txt"
+        
+        The default value of the instance name can be configured with the Set-AxActiveAosConfig cmdlet.
+        
+    .NOTES
+        Tags: XppIL, Hash, Files
+        
+        Author: Mötz Jensen (@Splaxi)
+        
+        All credits goes to Kenneth Madsen (@KennethGrupp) for providing detailed examples on how to achieve this the best way using powershell
+        
 #>
 
 function Get-AxServerXppILHash {
