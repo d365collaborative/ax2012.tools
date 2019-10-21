0c3ee3aa8107851b9e0f03f424ff1ae111b77595
Mötz Jensen <motz@e-s.dk>
2019-10-21 11:30:27 +0200

    Docs: CBH formatted

--- ax2012.tools/internal/functions/get-sid.ps1
+++ ax2012.tools/internal/functions/get-sid.ps1
@@ -1,23 +1,24 @@
-﻿<#
-.SYNOPSIS
-Get the Sid from the user
-
-.DESCRIPTION
-Translate the username into the unique sid of the user
-
+﻿
+<#
+    .SYNOPSIS
+        Get the Sid from the user
+        
+    .DESCRIPTION
+        Translate the username into the unique sid of the user
+        
     .PARAMETER Username
         User name with the domain name included, in either PRE-2000 or UPN style
-
-.EXAMPLE
-PS C:\> Get-Sid -Username "Test@ACME.LOCAL"
-
+        
+    .EXAMPLE
+        PS C:\> Get-Sid -Username "Test@ACME.LOCAL"
+        
         This will return the Sid from the user account "Test@ACME.LOCAL".
-
-.NOTES
-Tags:
-
-Author: Mötz Jensen (@Splaxi)
-
+        
+    .NOTES
+        Tags:
+        
+        Author: Mötz Jensen (@Splaxi)
+        
 #>
 
 function Get-Sid {
@@ -28,4 +29,3 @@ function Get-Sid {
 
     (New-Object System.Security.Principal.NTAccount($Username)).Translate([System.Security.Principal.SecurityIdentifier]).value
 }
-
