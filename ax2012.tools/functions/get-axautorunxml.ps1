a4fcc361cd2cd7bbfc4bb6a5d1729b9e8b096b96
Mötz Jensen <motz@e-s.dk>
2019-10-22 07:39:37 +0200

    Docs. CBH updated

--- ax2012.tools/functions/get-axautorunxml.ps1
+++ ax2012.tools/functions/get-axautorunxml.ps1
@@ -1,10 +1,59 @@
-﻿function Get-AxAutoRunXml {
+﻿<#
+.SYNOPSIS
+Get a valid Autorun XML file for AX 2012
+
+.DESCRIPTION
+Generate a valid Autorun XML file based on the selected parameters and have it saved on the disk
+
+.PARAMETER OutputPath
+The path where you want to store to autorun xml file
+
+The path must be a full path to a file
+
+.PARAMETER LogFile
+The path where you want the autorun process to save its log file
+
+The path must be a full path to a file
+
+.PARAMETER ExitWhenDone
+Instruct the autorun process wether you want it to exit the AX 2012 client when done or not
+
+.PARAMETER SynchronizeDB
+Instruct the autorun to do a synchronization of the database
+
+.PARAMETER CompileCilFull
+Instruct the autorun to do a full CIL compile
+
+.PARAMETER CompileCilIncremental
+Instruct the autorun to do a incremental CIL compile
+
+.PARAMETER CompileXpp
+Instruct the autorun to do a full X++ (Xpp) compile
+
+.PARAMETER CompileXppAndXRefUpdate
+Instruct the autorun to do a full X++ (Xpp) compile and do a full Cross Reference (XRef) update
+
+.EXAMPLE
+PS C:\> Get-AxAutoRunXml -ExitWhenDone -SynchronizeDB
+
+This will generate a valid Autorun xml file for synchronizing the database.
+It will use the default path "c:\temp\ax2012.tools\Autorun.xml" where the file will be stored.
+It will use the default path "c:\temp\ax2012.tools\Autorun_Output.txt" to instruct the Autorun where to save its log file.
+It will instruct the Autorun to exit when done.
+It will instruct the Autorun to do a synchronization of the database.
+
+.NOTES
+Tags:
+Author: Mötz Jensen (@Splaxi)
+#>
+
+function Get-AxAutoRunXml {
     [CmdletBinding()]
     param (
 
-        [String] $OutputPath,
+        [String] $OutputPath = (Join-Path $Script:DefaultTempPath "Autorun.xml"),
 
-        [String] $LogFile,
+        [String] $LogFile = (Join-Path $Script:DefaultTempPath "Autorun_Output.txt"),
 
         [Switch] $ExitWhenDone,
 
